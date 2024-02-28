2c5b89a0ccb6be81c22fcf7e59d8ad6ca0abc888
Do Phuong Anh <phuonganhnniie@Dos-MacBook-Air.local>
2024-02-28 15:35:30 +0700

    refactor: add console log to solution 513

--- leetcode/513/513.go
+++ leetcode/513/513.go
@@ -1,5 +1,7 @@
 package leetcode_513
 
+import "fmt"
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -12,6 +14,10 @@ type GraphNode struct {
 }
 
 func FindBottomLeftValue(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
 	bfsQueue := make([]GraphNode, 0)
 	bfsQueue = append(bfsQueue, GraphNode{root, 0})
 
@@ -22,17 +28,22 @@ func FindBottomLeftValue(root *TreeNode) int {
 		front := bfsQueue[0]
 		bfsQueue = bfsQueue[1:]
 
+		fmt.Printf("process node: %d at depth: %d\n", front.node.Val, front.depth)
+
 		if front.depth > currentDepth {
 			currentDepth = front.depth
 			leftMost = front.node.Val
+			fmt.Printf("new leftMost: %d at depth: %d\n", leftMost, currentDepth)
 		}
 
 		// enqueue children
 		if front.node.Left != nil {
+			fmt.Printf("enqueue left child of node %d\n", front.node.Val)
 			bfsQueue = append(bfsQueue, GraphNode{front.node.Left, front.depth + 1})
 		}
 
 		if front.node.Right != nil {
+			fmt.Printf("enqueue right child of node %d\n", front.node.Val)
 			bfsQueue = append(bfsQueue, GraphNode{front.node.Right, front.depth + 1})
 		}
 	}
