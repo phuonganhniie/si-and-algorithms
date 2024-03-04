5dd19b13b8d6ad02876b892813a38d3497c733dc
Do Phuong Anh <phuonganhnniie@Dos-MacBook-Air.netis.cc>
2024-03-04 23:26:52 +0700

    feat: solution 977 for easier way

--- leetcode/977/977.go
+++ leetcode/977/977.go
@@ -1,27 +1,39 @@
 package leetcode_977
 
-import "math"
+import "sort"
+
+// func SortedSquares(nums []int) []int {
+// 	n := len(nums)
+// 	left := 0
+// 	right, index := n-1, n-1
+
+// 	squaredArr := make([]int, n)
+
+// 	for index >= 0 {
+// 		leftVal := math.Abs(float64(nums[left]))
+// 		rightVal := math.Abs(float64(nums[right]))
+
+// 		if leftVal > rightVal {
+// 			squaredArr[index] = int(leftVal) * int(leftVal)
+// 			left++
+// 		} else {
+// 			squaredArr[index] = int(rightVal) * int(rightVal)
+// 			right--
+// 		}
+// 		index--
+// 	}
+
+// 	return squaredArr
+// }
 
 func SortedSquares(nums []int) []int {
 	n := len(nums)
-	left := 0
-	right, index := n-1, n-1
 
-	squaredArr := make([]int, n)
-
-	for index >= 0 {
-		leftVal := math.Abs(float64(nums[left]))
-		rightVal := math.Abs(float64(nums[right]))
-
-		if leftVal > rightVal {
-			squaredArr[index] = int(leftVal) * int(leftVal)
-			left++
-		} else {
-			squaredArr[index] = int(rightVal) * int(rightVal)
-			right--
-		}
-		index--
+	for i := 0; i < n; i++ {
+		num := nums[i] * nums[i]
+		nums[i] = num
 	}
 
-	return squaredArr
+	sort.Ints(nums)
+	return nums
 }
