605359c23770c8f8d4b73f256e84aeb2a5b94d08
Do Phuong Anh <phuonganhnniie@Dos-MacBook-Air.local>
2024-03-06 13:15:54 +0700

    feat: solution for problem 141 in Go using Map

--- leetcode/141/141.go
+++ leetcode/141/141.go
@@ -1,10 +1,16 @@
 package leetcode_141
 
+/**
+ * 141. The Cycle of Linked List
+ * https://leetcode.com/problems/linked-list-cycle/submissions/1195412987/?envType=daily-question&envId=2024-03-06
+ */
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
+// Two Pointer
 func HasCycle(head *ListNode) bool {
 	if head == nil || head.Next == nil {
 		return false
@@ -21,5 +27,23 @@ func HasCycle(head *ListNode) bool {
 		fastPtr = fastPtr.Next.Next
 	}
 
+	return false
+}
+
+// Map
+func HasCycleMap(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+
+	visitedMap := make(map[*ListNode]bool)
+	for head != nil {
+		if _, ok := visitedMap[head]; ok {
+			return true
+		}
+
+		visitedMap[head] = true
+		head = head.Next
+	}
 	return false
 }
