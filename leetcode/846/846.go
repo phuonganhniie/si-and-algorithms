4d69600b9162f6241047638310df5cbf789557c6
Anh <phuonganhniie@gmail.com>
2024-06-07 17:25:51 +0700

    chore: update template solve problem

--- leetcode/846/846.go
+++ leetcode/846/846.go
@@ -2,6 +2,27 @@ package leetcode_846
 
 import "sort"
 
+/*
+[Medium] 846. Hand of Straights
+https://leetcode.com/problems/hand-of-straights/description/
+Created: 2024-06-05
+Done   : 25 mins
+Attempt: 1
+---------------------NOTE---------------------
+Time: O(n log n)
+Space: O(n)
+
+Approach:
+1. Check for divisibility: If the number of cards isn't divisible by groupSize, it's impossible.
+2. Sort:  Make it easier to find consecutive groups.
+3. Frequency Map: Count occurrences of each card.
+4. Greedy Iteration:
++ Start with the smallest card.
++ For each card, check if it and the next (groupSize - 1) consecutive cards exist in the map.
++ If so, decrement their frequencies.
++ If not, return false.
+5. Success: If we iterate through all cards without returning false, it's possible to arrange the hand.
+*/
 func isNStraightHand(hand []int, groupSize int) bool {
 	if len(hand)%groupSize != 0 {
 		return false
