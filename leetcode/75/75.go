838cefb55864ee5cf9d54edc1ffa8115ddde6aac
Anh <phuonganhniie@gmail.com>
2024-06-12 23:47:09 +0700

    feat: solve problem 75 by Go

---
+++ leetcode/75/75.go
@@ -0,0 +1,55 @@
+package leetcode_75
+
+/*
+[Medium] 75. Sort Colors
+https://leetcode.com/problems/sort-colors/description/
+Created: 2024-06-12
+Done   : 10 mins 41 seconds
+Attempt: 3
+---------------------NOTE---------------------
+Time    : O(n log n)
+Space   : O(1)
+Approach: Heap Sort
+*/
+func sortColors(nums []int) []int {
+	n := len(nums)
+
+	// xây dựng max heap
+	// bắt đầu từ node con của root
+	for i := n/2 - 1; i >= 0; i-- {
+		heapify(nums, n, i)
+	}
+
+	for i := n - 1; i >= 0; i-- {
+		nums[0], nums[i] = nums[i], nums[0]
+
+		// tiếp tục heapify từ phần tử đầu tiên với mảng đã giảm đi phần tử cuối
+		// với mục đích xây dựng lại max-heap để đảm bảo phần tử root luôn lớn nhất
+		heapify(nums, i, 0)
+	}
+
+	result := nums
+	return result
+}
+
+func heapify(nums []int, n, i int) {
+	left := 2*i + 1
+	right := 2*i + 2
+	largest := i
+
+	if left < n && nums[left] > nums[largest] {
+		largest = left
+	}
+
+	if right < n && nums[right] > nums[largest] {
+		largest = right
+	}
+
+	// kiểm tra xem lúc này node thứ largest có phải node gốc không
+	// nếu không, đổi chỗ cho nhau và tiếp tục heapify
+	// (nghĩa là node con bên trái hoặc node con bên phải đang có giá trị lớn hơn node gốc)
+	if largest != i {
+		nums[largest], nums[i] = nums[i], nums[largest]
+		heapify(nums, n, largest)
+	}
+}
---
+++ leetcode/75/75_test.go
@@ -0,0 +1,30 @@
+package leetcode_75
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestSortColors(t *testing.T) {
+	tests := []struct {
+		nums []int
+		want []int
+	}{
+		{[]int{2, 0, 2, 1, 1, 0}, []int{0, 0, 1, 1, 2, 2}},
+		{[]int{2, 0, 1}, []int{0, 1, 2}},
+	}
+
+	countErr := 0
+	for _, tt := range tests {
+		got := sortColors(tt.nums)
+		if !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("sortColors test failed, want %v - got %v", tt.want, got)
+			fmt.Println("==================================")
+			countErr++
+		}
+	}
+	if countErr == 0 {
+		fmt.Println("Chúp mừng pà nkaaaa")
+	}
+}
