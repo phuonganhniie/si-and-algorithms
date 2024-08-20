66aaa6daee0460bfe04f3c9bc0442eb8ecd06d41
Anh <phuonganhniie@gmail.com>
2024-08-20 12:53:17 +0700

    solve cyclic rotation problem

---
+++ codility/cyclic_rotation.go
@@ -0,0 +1,16 @@
+package codility
+
+func Solution(A []int, K int) []int {
+	n := len(A)
+
+	if n == 0 || K == 0 {
+		return A
+	}
+
+	K = K % n
+
+	rs := A[n-K:]
+	remain := A[:n-K]
+	rs = append(rs, remain...)
+	return rs
+}
---
+++ codility/cyclic_rotation_test.go
@@ -0,0 +1,32 @@
+package codility
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	tests := []struct {
+		A    []int
+		K    int
+		want []int
+	}{
+		{[]int{3, 8, 9, 7, 6}, 3, []int{9, 7, 6, 3, 8}},
+		{[]int{1, 2, 3, 4}, 4, []int{1, 2, 3, 4}},
+		{[]int{0, 0, 0}, 1, []int{0, 0, 0}},
+	}
+
+	countErr := 0
+	for _, tt := range tests {
+		got := Solution(tt.A, tt.K)
+		if !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("Solution test failed, want %v - got %v", tt.want, got)
+			fmt.Println("==================================")
+			countErr++
+		}
+	}
+	if countErr == 0 {
+		fmt.Println("Chúp mừng pà nkaaaa")
+	}
+}
