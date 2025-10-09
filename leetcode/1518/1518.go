fe59db5a760ad00246c3379db818b2b4f699d837
Đỗ Phương Anh <24730003@ms.uit.edu.vn>
2025-10-09 22:01:43 +0700

    refactor: add more approach for 1518

--- leetcode/1518/1518.go
+++ leetcode/1518/1518.go
@@ -21,16 +21,40 @@ Time: O(n)
 Space: O(1)
 Approach: Simulation (này tự nghĩ ý tưởng nên cũng k biết theo pattern gì)
 */
-func numWaterBottles2(numBottles int, numExchange int) int {
-	consumedBottles := 0
+func numWaterBottles2(soChaiDangCo int, soVoCanDoi1Chai int) int {
+	daUong := 0
 
-	for numBottles >= numExchange {
-		numBottles -= numExchange
-		consumedBottles += numExchange
+	for soChaiDangCo >= soVoCanDoi1Chai {
+		soChaiDangCo -= soVoCanDoi1Chai
+		daUong += soVoCanDoi1Chai
 
-		// Exchange them for one full bottle
-		numBottles++
+		// Doi vo lay 1 chai moi
+		soChaiDangCo++
 	}
 
-	return consumedBottles + numBottles
+	return daUong + soChaiDangCo
+}
+
+/*
+---------------------NOTE---------------------
+Time: O(n)
+Space: O(1)
+Approach: Cách của empe, ún xong một lần rồi chạy đi đổi
+*/
+func numWaterBottles3(numBottles int, numExchange int) int {
+	totalDrank := 0
+	full := numBottles
+	empties := 0
+
+	for full > 0 {
+		// Uống hết số chai đầy hiện có
+		totalDrank += full
+		empties += full
+
+		// Đổi vỏ lấy chai đầy mới
+		full = empties / numExchange
+		empties = empties % numExchange
+	}
+
+	return totalDrank
 }
